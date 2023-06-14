cc3c42e947b6e68179e634f6ddfdfd98c724cbf3
Dominik Gumiński <drgt344@hotmail.com>
2023-06-14 19:19:46 +0200

    fix wrong token padding

--- ChatAPI.Application/Common/Services/TokenManager.cs
+++ ChatAPI.Application/Common/Services/TokenManager.cs
@@ -10,34 +10,34 @@ public interface ITokenManager
     string GenerateToken(IReadOnlyDictionary<string, StringValues> claims);
     IReadOnlyDictionary<string, StringValues> ValidateToken(string token);
 }
-
-/// -----BEGIN PUBLIC KEY-----
-/// MIGeMA0GCSqGSIb3DQEBAQUAA4GMADCBiAKBgFRcG7oBHwN4GPYvTg7yFajmokzz
-/// SijTrBvRGrbE3c54boxN8TzWUt+i8r/fqMIjQcBJSBA4h5nnZMkrfA7u44xpJfND
-/// O/GGlgeODffZII652VisYQXzj+tAOOfoOcZAITzI6idOut2Z2Mxw9a/xC5jla4bs
-/// g/H91KOm0zzgsNgjAgMBAAE=
-/// -----END PUBLIC KEY-----
+/* -----BEGIN PUBLIC KEY-----
+ * MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC+JylwQKKpe6zKJCp0vgAWrJfa
+ * DX4vfKmr/MXa5wNb/lIf78AQLSub4KNX/Olc+FVYM8pCjwI8pA9IWqdDdTWjZd+D
+ * 3rNF8LXoOKzsROcGCpoLspbvkxBFahzf64mAHSuJfJVrsgbajLMIW0su2houSS/0
+ * UAzre24Ic6NDjYNVKwIDAQAB
+ * -----END PUBLIC KEY-----
+ */
 public sealed class TokenManager : ITokenManager
 {
     private const int KEY_BITS = 1024;
     private readonly static string _privateKey = """
         -----BEGIN RSA PRIVATE KEY-----
-        MIICWgIBAAKBgFRcG7oBHwN4GPYvTg7yFajmokzzSijTrBvRGrbE3c54boxN8TzW
-        Ut+i8r/fqMIjQcBJSBA4h5nnZMkrfA7u44xpJfNDO/GGlgeODffZII652VisYQXz
-        j+tAOOfoOcZAITzI6idOut2Z2Mxw9a/xC5jla4bsg/H91KOm0zzgsNgjAgMBAAEC
-        gYAtQ8MI2iMm/GEAb7+Fm3Xty9rYSU7Ie1OFFX0tBpMxf0Np+0Ru7V1IbCmutLbb
-        fImCQI/vTDXOae+VVcTaSmpDxhSwivo7Dl348UlOVYk4rkGbdQr/Xg9N3MfINpm5
-        x+uXT9GZGretIcRxO4fKLjH8K3Jx8eiN2Ti40ZMSNcxpuQJBAJsRYk1hYkaUeBQi
-        1YPup4E2Rp688hsRfkyqKj7R7GrOfcQj+feBwG3eQPx8FnXTBm0pL33sqzCmiLG0
-        GWQweIUCQQCLRMqewCem2ZdYEJl+D4SyrvuNr1gLQmraqaXNcSMqvl8aJDhjsCDA
-        FAC2rcjQ1ubGoXsFa8BdLlLK7YFgg0KHAkBtmv0DuujPAJRbjz+iMGcfcrC59M2g
-        Cl5ebAzOOG1GFUxZ/h/qLUFJp0YB8OejQpSRRgI2nLln+t411Rn5cjVNAkBgBaQ2
-        ZIJyVeA1hexusEBr+p3SiK0JxldqQEHjLjhzBiMIISUIBq3uAVykl5m39BPVrAzo
-        JezR0lifNbZYVugNAkAp9ojRO0z4896gaZW7XAb100FGQcnMvTipC8dAI6Z1AjII
-        Aub7AQQw57iNdXG3mATXCVhVwloBnuhhJOWPJfrN
+        MIICXgIBAAKBgQC+JylwQKKpe6zKJCp0vgAWrJfaDX4vfKmr/MXa5wNb/lIf78AQ
+        LSub4KNX/Olc+FVYM8pCjwI8pA9IWqdDdTWjZd+D3rNF8LXoOKzsROcGCpoLspbv
+        kxBFahzf64mAHSuJfJVrsgbajLMIW0su2houSS/0UAzre24Ic6NDjYNVKwIDAQAB
+        AoGBAJqUwYYktVdsV/p4Th9bejz0j/nOsD8wa8qKEdozpLJ7XA3kXFGKNwJgKsnT
+        q25N3yt15r4W/e2IPXhHYeRf+3pzjbPhVEyfEE/yCvybVAqvPmxnYTXAWa6lJDXt
+        D0wd4+UGGDW/7Rnk5/N6jXnVoacHEcpaXBQrWYtj+iHN7DBpAkEA6OZeKgjkNbbs
+        56/3bvzAeyREUiLtfkedl6rK8UUK8jT4h6DlVT/gnt1Ouk9HkSzqCe1B/+Gc/L+c
+        HZtrzkL0TQJBANEDZPlX+Fuzu/M7V6VKGWovxAiL0Q/aClOys0UYhOw6YDlNK2z7
+        DfdXDUcptpNHOVr3rEDUTG+r8eycDTLPC1cCQQDeyE5W4z55a0verGKB1mDA6oY4
+        E5UecJ79D7Elbaf03FrIUk5NZ9cT4BqI+YE6C76sVDoH7ObgluFdKOjM2xsZAkA8
+        2PchW8nSsH1r7v+x/+NsNWGld0ayjbBp82EbIWs49jmjFOMqg9/p/K7B49PiBl8d
+        K3M7IFLsQ6tXTYrOGtZbAkEAyr4DE9Gcx3d2MpR8H6TE56LKQwOrwn9vEYk2/CFh
+        Wx5g8k2emf6qoAJx6/uX27V8fMT8KrZqsXZDR0D9AAeqCA==
         -----END RSA PRIVATE KEY-----
         """;
-    private readonly static RSAEncryptionPadding _padding = RSAEncryptionPadding.OaepSHA512;
+    private readonly static RSAEncryptionPadding _padding = RSAEncryptionPadding.Pkcs1;
     public string GenerateToken(IReadOnlyDictionary<string, StringValues> claims)
     {
         var serialisedClaims = JsonSerializer.Serialize(claims);
